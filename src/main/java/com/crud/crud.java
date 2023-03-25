37e09372ef8fc903f74bd67158557249863e7ed7
josemar <junioraguiar255@gmail.com>
2023-03-25 20:29:47 -0300

    inserindo mensagem na primeira pagina

--- src/main/java/com/crud/crud.java
+++ src/main/java/com/crud/crud.java
@@ -2,9 +2,19 @@ package com.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+
+
 public class crud {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Bem vindo na minha aplicação!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(crud.class, args);
